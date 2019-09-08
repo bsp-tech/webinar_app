f0731fab0b6578bc65dd4bbbe4676bd144f515e7
azermurad1i <azermuradli1@gmail.com>
2019-09-08 22:37:54 +0500

    home page

--- screens/homescreen/welcomeNavigation/WelcomeNavigation.js
+++ screens/homescreen/welcomeNavigation/WelcomeNavigation.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import ThumbnailText from '../../../components/ThumbnailText';
 import { View } from 'native-base';
 import ViewRow from '../../../components/grid/ViewRow';
-import {Dimensions} from 'react-native'
+import {Dimensions,Text,TouchableOpacity,StyleSheet} from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+import Video from "react-native-video"
 import UIButton from '../../../components/ui/UIButton';
 import { withNavigation } from 'react-navigation';
 import images from '../../../assets/icons/base64images';
@@ -15,34 +17,45 @@ class WelcomeNavigation extends Component {
     render() {
         const {navigation}  = this.props;
         return (
-            <View style={{alignSelf:"center",height:500, width:containerWidth, justifyContent:"space-between"}}>
-                <ViewRow >
-                    <ThumbnailText fontSize={10} width={80} height={80} text={"1. Choose region"} 
-                    base64
-                    images={[images.image1]} />
-                    <ThumbnailText fontSize={10} width={80} height={80} text={"2. Choose dates"}
-                     images={[images.image2]}  />
-                </ViewRow>
-                <ViewRow >
-                    <ThumbnailText fontSize={10} width={80} height={80} text={"3. Choose vehicle"} 
-                    images={[images.image3]}  />
-                    <ThumbnailText fontSize={10} width={80} height={80} text={"4. Choose guide"} 
-                    images={[images.image4]}  />
-                </ViewRow>
-                <ViewRow>
-                     <ThumbnailText fontSize={10} width={80} height={80} text={"5. Invoice"} 
-                    images={[images.image6]}  />
-                    <ThumbnailText fontSize={10} width={80} height={80} text={"6. Pay by cash"} 
-                    images={[images.image5]}  />
-                </ViewRow>
-                        
+            <View style={{}}>
+                <View style={{justifyContent:"center",height:60,maxHeight:100,backgroundColor:"#0988b3"}}>
+                <Text style={{textAlign:"center",width:containerWidth,alignSelf:"center",color:"white"}}>An element of truth - videos about science, education, and anything else I find interesting.</Text>      
+                </View>
+                <View style={{justifyContent:"center",alignItems:"center",paddingVertical:10,alignSelf:"center",width:containerWidth,color:"white"}}>
+                <Ionicons name="logo-youtube" size={32} color="red" />
+                  <Text style={{}}>LIVE STREAM</Text>
+                  <Text style={{textAlign:"center",fontSize:20}}>We are going to speak about evolution tomorrow in a live broadcast </Text>
+                </View>
+               
+                <View style={{justifyContent:"center",height:60,maxHeight:100,backgroundColor:"#0988b3"}}>
+                     <Text style={{textAlign:"center",width:containerWidth,alignSelf:"center",color:"white"}}>An element of truth - videos about science, education, and anything else I find interesting.</Text>      
+                  </View>
+                 
+
+              
                 <UIButton onPress={() => {
                         navigation.navigate('DestinationList')
                     }}
-                    text="EXPLORE" style={{marginTop:40, marginBottom:40}}></UIButton>
+                    text="EXPLORE" style={{marginTop:40, marginBottom:20}}></UIButton>
+                <TouchableOpacity  onPress={() => {
+                        navigation.navigate('Settings')
+                    }}>
+                    <Text style={{textAlign:"center"}}>Contact us</Text>
+                </TouchableOpacity>
             </View>
         )
     }
 }
+
+var styles = StyleSheet.create({
+    backgroundVideo: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+    },
+  });
+
 
 export default withNavigation(WelcomeNavigation)
